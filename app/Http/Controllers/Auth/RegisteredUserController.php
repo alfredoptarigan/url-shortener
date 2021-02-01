6d8ee093b811cbdff3f69a164c87828569e5f3a8
Alfredo Patricius Tarigan <62396803+alfredoptarigan@users.noreply.github.com>
2021-02-01 20:03:21 +0700

    Nullable Fix Image

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -45,7 +45,7 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
-            'avatar' => $request->avatar !== null ? $this->UploadImage($request->avatar) : ""
+            'avatar' => $request->avatar !== null ? $this->UploadImage($request->avatar) : null
         ]));
 
         event(new Registered($user));
